6c0277d3a83e9ed3082d2bb31efeff248b5f3023
Prashant Solanki <prashant.sanki@gmail.com>
2016-06-05 04:44:02 -0400

    updated for new changes

--- DaytSEDownload.user.js
+++ DaytSEDownload.user.js
@@ -2,8 +2,8 @@
 // @name        DaytSEDownload | Sanki
 // @namespace   https://github.com/LordSanki/WebGrease/
 // @description Creates a download button on datse webpage
-// @include     http://dayt.se/forum/showthread.php*
-// @version     2.1
+// @include     http://dayt.se/*
+// @version     2.2
 // @grant       none
 // @noframes
 // @encoding utf-8
@@ -12,62 +12,65 @@
 // @downloadURL https://github.com/LordSanki/WebGrease/raw/master/DaytSEDownload.user.js
 // @homepage https://github.com/LordSanki/WebGrease
 // ==/UserScript==
-
 /*=======================================================
   Script
 ======================================================*/
-
-function DaytSEDownload(){
-   var link = undefined;
-   // take an educated guess to finding link
-   $('iframe').each( function(index){
-     var src = $(this).attr('src');
-     if( src.match(/dayt\.se\/pasmov/g) || src.match(/dayt\.se\/pasep/g)){
-       var l1frame = $(this).contents();
-       var l2frame = $(l1frame).find('#ggplayer');
-       link = $(l2frame).contents().find('iframe').attr('src');
-     }
-   });
-  if(link == undefined){
-    console.log("Brutefoce");
-    RecursiveSearchForLink('body',1);
-  }
-  else{
+function DaytSEDownload() {
+  var link = undefined;
+  // take an educated guess to finding link
+  $('iframe').each(function (index) {
+    var src = $(this).attr('src');
+    if(src != undefined){
+    if (src.match(/dayt\.se\/pasmov/g) || src.match(/dayt\.se\/pasep/g)) {
+      var l1frame = $(this).contents();
+      var l2frame = $(l1frame).find('#ggplayer');
+      link = $(l2frame).contents().find('iframe').attr('src');
+    }
+    }
+  });
+  if (link == undefined) {
+    console.log('Brutefoce');
+    RecursiveSearchForLink('body', 1);
+  } 
+  else {
     DownloadWithLink(link);
   }
 }
-
-function DownloadWithLink(link){
-   link = link.replace("file\/d\/", "uc\?id=");
-   link = link.replace("\/preview", "&export=download");
-   console.log(link);
-   window.open(link, "_newtab");
+function DownloadWithLink(link) {
+  link = link.replace('file/d/', 'uc?id=');
+  link = link.replace('/preview', '&export=download');
+  console.log(link);
+  window.open(link, '_newtab');
 }
-
-function RecursiveSearchForLink(elem, lvl){
-  $(elem).find('iframe').each( function(index){
-    console.log("LVL "+lvl+">");
-    console.log(":::\t" + $(this).attr('src'));
-    if($(this).attr('src') == undefined) {return;}
-    if($(this).attr('src').match(/docs\.google\.com\/file/g)){
-      DownloadWithLink($(this).attr('src'));
-      console.log("Found");
+function RecursiveSearchForLink(elem, lvl) {
+  $(elem).find('iframe').each(function (index) {
+    console.log('LVL ' + lvl + '>');
+    console.log(':::\t' + $(this).attr('src'));
+    if ($(this).attr('src') == undefined) {
       return;
     }
-    // reject cross domain iframes
-    if(!$(this).attr('src').match(/http\:\/\/dayt\.se/g)){return;}
-    // reject iframes with no child iframes
-    if($(this).contents().find('iframe')[0] == undefined){return;}
-    var t = RecursiveSearchForLink($(this).contents(), lvl+1);
+    if ($(this).attr('src').match(/docs\.google\.com\/file/g)) {
+      DownloadWithLink($(this).attr('src'));
+      console.log('Found');
+      return;
+    } // reject cross domain iframes
+
+    if ( (!$(this).attr('src').match(/http\:\/\/dayt\.se/g))&&
+         ($(this).attr('src').match(/http/g)) ) {
+      return;
+    } // reject iframes with no child iframes
+
+    if ($(this).contents().find('iframe') [0] == undefined) {
+      return;
+    }
+    var t = RecursiveSearchForLink($(this).contents(), lvl + 1);
   });
 }
-
-window.onload = function(){
-    $('#forumheadercontent > #navbar > font').append("<button onclick='DaytSEDownload()'>Download</button>");
-   var script = document.createElement('script');
-   script.appendChild(document.createTextNode( DaytSEDownload ));
-   script.appendChild(document.createTextNode( RecursiveSearchForLink ));
-   script.appendChild(document.createTextNode( DownloadWithLink ));
-   (document.body || document.head || document.documentElement).appendChild(script);
+window.onload = function () {
+  $('#navbar > font').append('<button onclick=\'DaytSEDownload()\'>Download</button>');
+  var script = document.createElement('script');
+  script.appendChild(document.createTextNode(DaytSEDownload));
+  script.appendChild(document.createTextNode(RecursiveSearchForLink));
+  script.appendChild(document.createTextNode(DownloadWithLink));
+  (document.body || document.head || document.documentElement).appendChild(script);
 }
-
